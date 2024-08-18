6e85da4faf4509e5f5679bb2bbf3ec8a09d7da89
ashwinn-si <ashwin23cse@gmail.com>
2024-08-18 09:12:36 +0530

    DONE...!

---
+++ 18-08-DT-Even Integers - Shifting Digits.py
@@ -0,0 +1,23 @@
+'''The must accept an integer N as the input The program must print the even integers
+that can be formed by shifting the digits to the right in N.
+- At least 1 even digit is always present in N.
+• The resulting even integers must be printed without leading zeros.
+Example InBJt/Output :
+Input
+4763
+3476 7634
+'''
+
+def even_number_checker(n):
+    if(n%2==0):
+        return True
+    else:
+        return False
+number=input().strip()
+len_number=len(number)
+for i in range(len_number):
+    num=int(number)
+    if(even_number_checker(num)):
+        print(num,end=" ")
+    number=number[-1]+number[:len_number-1]
+    
