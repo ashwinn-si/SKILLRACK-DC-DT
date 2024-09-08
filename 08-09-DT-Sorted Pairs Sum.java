c8f9e965fa4425410b154a27ee3b07a0eaa16b20
ashwinn-si <ashwin23cse@gmail.com>
2024-09-08 15:56:25 +0530

    Updated....

--- 08-09-DT-Sorted Pairs Sum.java
+++ 08-09-DT-Sorted Pairs Sum.java
@@ -2,7 +2,7 @@
 present in the given N integers. Then the program must sort the pairs based on the sum. If two or more pairs having the
 same sum, then the program must sort those pairs based on the value of the first operand. Finally, the program must print
 the sorted pairs based on the following format.
-Operandl + Operand2 = Sum
+Operand1 + Operand2 = Sum
 If there is no such pair, then the program must print -1 as the output.
 
 Input:
@@ -43,7 +43,7 @@ public class Hello {
 		List<List<Integer>> pairs=new ArrayList<>();
 		for(int i=0;i<N;i++){
 		    for(int j=0;j<N;j++){
-		        if(i != j && num.contains(num.get(i)+num.get(j))){
+		        if(i != j && num.contains(num.get(i)+num.get(j))){ 
 		            List<Integer> curr_pair=new ArrayList<>();
 		            curr_pair.add(num.get(i));
 		            curr_pair.add(num.get(j));
@@ -60,17 +60,15 @@ public class Hello {
 	    Comparator<List<Integer>> comp1= new Comparator<List<Integer>>(){
 	        @Override
 	        public int compare(List<Integer> a1,List<Integer> a2){
-	            return Integer.compare(a1.get(0),a2.get(0));
-	        }
-	    };
-	    Comparator<List<Integer>> comp2=new Comparator<List<Integer>>(){
-	        @Override
-	        public int compare(List<Integer> a1,List<Integer>a2){
-	            return Integer.compare(a1.get(2),a2.get(2));
+				if(a1.get(2) != a2.get(2)){
+					return Integer.compare(a1.get(2),a2.get(2));
+				}else if(a1.get(1) !=a2.get(1)){
+					return Integer.compare(a1.get(0),a2.get(0));
+				}
+	            
 	        }
 	    };
 	    Collections.sort(pairs,comp1);
-	    Collections.sort(pairs,comp2);
 	    for(int i=0;i<pairs.size();i++){
 	        System.out.printf("%d + %d = %d\n",pairs.get(i).get(0),pairs.get(i).get(1),pairs.get(i).get(2));
 	    }
