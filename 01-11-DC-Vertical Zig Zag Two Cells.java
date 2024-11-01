cbbc58f0e5f9f91df05590ffa421854930b0b8c1
ashwinn-si <ashwin23cse@gmail.com>
2024-11-01 08:57:18 +0530

    DONE...!

---
+++ 01-11-DC-Vertical Zig Zag Two Cells.java
@@ -0,0 +1,74 @@
+/*The program must accept a character matrix of size RxC arri the positions of two cells Cl
+YI), Q (X2. Y2) as the input The program must print the characters from the cell Cl to the cell
+C2 in vertical zig-zag direction. The vertical zig-zag traversal from the cell Cl to the cell C2 is
+explained below.
+- The traversal always begins from the cell Cl and it moves towards the bottom in the same
+column K.
+- Then it rnoves from bottom to top in the next column 1.
+- Then it rnoves from top to bottom in the next column K+2.
+Similarly. it moves in vertical zig.zag direction until it reaches the cell Q.
+
+Input
+5 7
+C m D k i t d
+E t q C e o g
+i A q q b c D
+F y m z g g o
+g h a I A I s
+4 2
+3 6
+
+Output
+yhamqqDkCqzIAgbeitoc
+*/
+
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner sc = new Scanner(System.in);
+		int R = sc.nextInt();
+		int C = sc.nextInt();
+		sc.nextLine();
+		char matrix[][] = new char[R][C];
+		for(int i = 0 ; i < R ; i++){
+		    for(int j = 0 ; j < C ; j++){
+		        matrix[i][j] = sc.next().charAt(0);
+		    }
+		}
+		int stR = sc.nextInt()-1;
+		int stC = sc.nextInt()-1;
+		sc.nextLine();
+		int endR = sc.nextInt()-1;
+        int endC = sc.nextInt()-1;
+        boolean flag=true;
+        while(true){
+            if(!printer(stR , stC , flag , endR , endC , R , matrix)){
+                System.out.printf("%c",matrix[endR][endC]);
+                break;
+            }
+            if(flag){ // flag -> true [moving from up to down] , flag -> false [moving from down to up]
+                flag = false;
+                stR = R-1; 
+            }else{
+                flag = true;
+                stR = 0;
+            }
+            stC++;
+            
+        }
+        
+	}
+	static boolean printer(int currR , int currC , boolean flag , int targetR , int targetC , int R , char matrix[][]){
+	   int increment = (flag) ? 1 : -1 ;
+	   while(currR >= 0  && currR < R && currC>=0 && (currC != targetC || currR != targetR )){
+	       System.out.printf("%c",matrix[currR][currC]);
+	       currR+=increment;
+	   }
+	   if(currC == targetC && currR == targetR){
+	       return false;
+	   }
+	   return true;
+	}
+}
